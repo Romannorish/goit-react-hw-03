9202384c52b78b1dae3ed744dd4d7fc42cb3ff2b
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2024-03-08 17:33:30 +0200

    updata nano id
    updata

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import css from "./App.module.css";
 import ContactList from "./ContactList/ContactList";
 import SearchBar from "./SearchBar/SearchBar";
 import ContactForm from "./ContactForm/ContactForm";
-// import {nanoid} from "nanoid";
+import {nanoid} from "nanoid";
+
 function App() {
   const [contactsData, setContactsData] = useState([
     {id: "id-1", name: "Rosie Simpson", number: "459-12-56"},
@@ -11,7 +12,7 @@ function App() {
     {id: "id-3", name: "Eden Clements", number: "645-17-79"},
     {id: "id-4", name: "Annie Copeland", number: "227-91-26"},
   ]);
-
+  const [serch, setSerch] = useState("");
   useEffect(() => {
     localStorage.setItem("contactsData", JSON.stringify(contactsData));
   }, [contactsData]);
@@ -21,24 +22,15 @@ function App() {
   };
   const addContact = (newContact) => {
     setContactsData((initContacts) => {
+      newContact.id = nanoid();
       return [...initContacts, newContact];
     });
   };
-  const [serch, setSerch] = useState("");
 
   const onSerch = contactsData.filter((contact) =>
     contact.name.toLowerCase().includes(serch.toLowerCase())
   );
 
-  // const addNewContact = (contData) => {
-  //   const finalContact = {
-  //     ...contData,
-  //     id: nanoid(),
-  //   };
-  //   setContactsData((prevState) => {
-  //     [...prevState, finalContact];
-  //   });
-  // };
   return (
     <>
       <div className={css.containerApp}>
